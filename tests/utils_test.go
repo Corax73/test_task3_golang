2fe33c006a5d4196bb65532ef0c22a1ebb1cfd8b
Corax73 <ramaz999@inbox.ru>
2025-06-12 23:48:29 +0400

    added tests

--- tests/utils_test.go
+++ tests/utils_test.go
@@ -25,3 +25,34 @@ func TestGetConfFromEnvFile(t *testing.T) {
 		t.Log("Done with incorrect filename")
 	}
 }
+
+func TestConcatSlice(t *testing.T) {
+	errStr := "321"
+	correctStr := "123"
+	if errStr != utils.ConcatSlice([]string{"1", "2", "3"}) {
+		t.Log("Done with incorrect comparison")
+	} else {
+		t.Errorf("Result was incorrect with incorrect comparison")
+	}
+	if correctStr == utils.ConcatSlice([]string{"1", "2", "3"}) {
+		t.Log("Done with correct comparison")
+	} else {
+		t.Errorf("Result was correct with incorrect comparison")
+	}
+}
+
+func TestCompareMapsByStringKeys(t *testing.T) {
+	map1 := map[string]string{"1": "1", "2": "2"}
+	map2 := map[string]string{"1": "1", "2": "2"}
+	map3 := map[string]string{"1": "1", "2": "2", "3": "3"}
+	if utils.CompareMapsByStringKeys(map1, map2) {
+		t.Log("Done with identical maps")
+	} else {
+		t.Errorf("Result was incorrect with identical maps")
+	}
+	if !utils.CompareMapsByStringKeys(map1, map3) {
+		t.Log("Done with unequal maps")
+	} else {
+		t.Errorf("Result was incorrect with unequal maps")
+	}
+}
