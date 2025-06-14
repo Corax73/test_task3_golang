3cc71d2e1b9f6068d9620d496a4d29e7b32edd2f
Corax73 <ramaz999@inbox.ru>
2025-06-14 23:13:00 +0400

    added tests

--- tests/utils_test.go
+++ tests/utils_test.go
@@ -2,6 +2,7 @@ package utilstest
 
 import (
 	"checklist/utils"
+	"slices"
 	"testing"
 )
 
@@ -54,5 +55,88 @@ func TestCompareMapsByStringKeys(t *testing.T) {
 		t.Log("Done with unequal maps")
 	} else {
 		t.Errorf("Result was incorrect with unequal maps")
+	}
+}
+
+func TestGetMapKeysWithValue(t *testing.T) {
+	map1 := map[string]string{"1": "1", "2": "2"}
+	map2 := map[string]string{"1": "", "2": "2"}
+	if len(utils.GetMapKeysWithValue(map1)) == 2 {
+		t.Log("Done with a map with two non-empty values")
+	} else {
+		t.Errorf("Result was incorrect with a map with two non-empty values")
+	}
+	if len(utils.GetMapKeysWithValue(map2)) == 1 {
+		t.Log("Done with a map with one non-empty values")
+	} else {
+		t.Errorf("Result was incorrect with a map with one non-empty values")
+	}
+}
+
+func TestGetMapValues(t *testing.T) {
+	map1 := map[string]string{"1": "1", "2": "2"}
+	map2 := map[string]string{"1": "", "2": "2"}
+	testSlice1 := utils.GetMapValues(map1)
+	var hasErr bool
+	for _, val := range map1 {
+		if !slices.Contains(testSlice1, val) {
+			t.Errorf("Result was incorrect for value %s of map1", val)
+			hasErr = true
+		}
+	}
+	if !hasErr {
+		t.Log("Done with a map with map1")
+	}
+	testSlice2 := utils.GetMapValues(map2)
+	hasErr = false
+	for _, val := range map2 {
+		if !slices.Contains(testSlice2, val) && val != "" {
+			t.Errorf("Result was incorrect for value %s of map2", val)
+			hasErr = true
+		}
+	}
+	if !hasErr {
+		t.Log("Done with a map with map2")
+	}
+}
+
+func TestGetIndexByStrValue(t *testing.T) {
+	testSlice := []string{"1", "2"}
+	if utils.GetIndexByStrValue(testSlice, "2") == 1 {
+		t.Log("Done with a map with correct index")
+	} else {
+		t.Errorf("Result was incorrect with correct index")
+	}
+	if utils.GetIndexByStrValue(testSlice, "2") == 0 {
+		t.Errorf("Result was incorrect with incorrect index")
+	} else {
+		t.Log("Done with a map with incorrect index")
+	}
+}
+
+func TestGetMapKeys(t *testing.T) {
+	map1 := map[string]string{"1": "1", "2": "2"}
+	keys1 := []string{"1", "2"}
+	map2 := map[string]string{"3": "3", "4": "4"}
+	keys2 := []string{"3", "4"}
+	var hasErr bool
+	for key, val := range map1 {
+		if !slices.Contains(keys1, key) {
+			t.Errorf("Result was incorrect for key %s of map1", val)
+			hasErr = true
+		}
+	}
+	if !hasErr {
+		t.Log("Done with a map with map1")
+	}
+	hasErr = false
+	for key, val := range map2 {
+		if !slices.Contains(keys2, key) {
+			t.Errorf("Result was incorrect for key %s of map2", val)
+			hasErr = true
+		}
+	}
+	if !hasErr {
+		t.Log("Done with a map with map2")
 	}
 }
