fd3b04b1ecf9ed6b1f0d1f0b190f265f7dbb5d4d
Corax73 <ramaz999@inbox.ru>
2025-06-16 23:21:21 +0400

    added tests

--- tests/utils_test.go
+++ tests/utils_test.go
@@ -116,9 +116,9 @@ func TestGetIndexByStrValue(t *testing.T) {
 
 func TestGetMapKeys(t *testing.T) {
 	map1 := map[string]string{"1": "1", "2": "2"}
-	keys1 := []string{"1", "2"}
+	keys1 := utils.GetMapKeys(map1)
 	map2 := map[string]string{"3": "3", "4": "4"}
-	keys2 := []string{"3", "4"}
+	keys2 := utils.GetMapKeys(map2)
 	var hasErr bool
 	for key, val := range map1 {
 		if !slices.Contains(keys1, key) {
@@ -138,5 +138,100 @@ func TestGetMapKeys(t *testing.T) {
 	}
 	if !hasErr {
 		t.Log("Done with a map with map2")
+	}
+}
+
+func TestPresenceMapKeysInOtherMap(t *testing.T) {
+	map1 := map[string]string{"1": "1", "2": "2"}
+	map2 := map[string]string{"1": "1", "2": "2"}
+	map3 := map[string]string{"3": "3", "4": "4"}
+	if utils.PresenceMapKeysInOtherMap(map1, map2) {
+		t.Log("Done with a map with map1 and map2")
+	} else {
+		t.Errorf("Result was incorrect with map1 and map2")
+	}
+	if utils.PresenceMapKeysInOtherMap(map1, map3) {
+		t.Errorf("Result was incorrect with map1 and map3")
+	} else {
+		t.Log("Done with a map with map1 and map3")
+	}
+}
+
+func TestGetMapWithoutKeys(t *testing.T) {
+	map1 := map[string]string{"1": "1", "2": "2", "3": "3"}
+	newMap1 := utils.GetMapWithoutKeys(map1, []string{"1"})
+	var hasErr bool
+	if _, ok := newMap1["1"]; ok {
+		hasErr = true
+	}
+	if _, ok := newMap1["2"]; !ok {
+		hasErr = true
+	}
+	if _, ok := newMap1["3"]; !ok {
+		hasErr = true
+	}
+	if hasErr {
+		t.Errorf("Result was incorrect with existing key")
+	} else {
+		t.Log("Done with a map with existing key")
+	}
+	hasErr = false
+	newMap2 := utils.GetMapWithoutKeys(map1, []string{"4"})
+	if _, ok := newMap2["1"]; !ok {
+		hasErr = true
+	}
+	if _, ok := newMap2["2"]; !ok {
+		hasErr = true
+	}
+	if _, ok := newMap2["3"]; !ok {
+		hasErr = true
+	}
+	if hasErr {
+		t.Errorf("Result was incorrect with a non-existent key")
+	} else {
+		t.Log("Done with a map with a non-existent key")
+	}
+}
+
+func TestIsEmail(t *testing.T) {
+	validEmailStr := "123_%b@123.com"
+	invalidEmailStr1 := "123_%B@123.com"
+	invalidEmailStr2 := "123_%B123.com"
+	invalidEmailStr3 := "123_%Я@123.com"
+	if utils.IsEmail(validEmailStr) {
+		t.Log("Done with a map with with validEmailStr")
+	} else {
+		t.Errorf("Result was incorrect with validEmailStr")
+	}
+	if utils.IsEmail(invalidEmailStr1) {
+		t.Errorf("Result was incorrect with invalidEmailStr1")
+	} else {
+		t.Log("Done with a map with with invalidEmailStr1")
+	}
+	if utils.IsEmail(invalidEmailStr2) {
+		t.Errorf("Result was incorrect with invalidEmailStr2")
+	} else {
+		t.Log("Done with a map with with invalidEmailStr2")
+	}
+	if utils.IsEmail(invalidEmailStr3) {
+		t.Errorf("Result was incorrect with invalidEmailStr3")
+	} else {
+		t.Log("Done with a map with with invalidEmailStr3")
+	}
+}
+
+func TestCheckPasswordHash(t *testing.T) {
+	passwordValid := "123abc"
+	passwordInvalid := "123abc1"
+	passwordHash, _ := utils.HashPassword(passwordValid)
+	if utils.CheckPasswordHash(passwordValid, passwordHash) {
+		t.Log("Done with passwordValid")
+	} else {
+		t.Errorf("Result was incorrect with passwordValid")
+	}
+	if utils.CheckPasswordHash(passwordInvalid, passwordHash) {
+		t.Errorf("Result was incorrect with passwordInvalid")
+	} else {
+		t.Log("Done with passwordInvalid")
 	}
 }
