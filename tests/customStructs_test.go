b4a59fb165024d35e1fb9b1ba6d92bc8a1e50be5
Corax73 <ramaz999@inbox.ru>
2025-06-22 23:28:42 +0400

    added tests

---
+++ tests/customStructs_test.go
@@ -0,0 +1,39 @@
+package app_test
+
+import (
+	"checklist/customStructs"
+	"testing"
+)
+
+func TestToString(t *testing.T) {
+	simpleResponse := customStructs.ListResponse{
+		Success: true,
+		Message: []map[string]any{
+			{
+				"key1": 1,
+				"key2": "2",
+				"key3": true,
+			},
+			{
+				"key1": 1,
+				"key2": 2,
+				"key3": []int{1, 2, 3},
+			},
+			{
+				"key1": map[string]int{"1": 1, "2": 2, "3": 3},
+			},
+		},
+	}
+	validString := `[{"key1":1,"key2":"2","key3":true},{"key1":1,"key2":2,"key3":[1,2,3]},{"key1":{"1":1,"2":2,"3":3}}]`
+	inValidString := `[{"key1":1,"key2":"2","key3":true},{"key1":1,"key2":2,"key3":[1,2,3]}]`
+	if simpleResponse.ToString() == validString {
+		t.Log("Done with validString")
+	} else {
+		t.Errorf("Result was incorrect with validString")
+	}
+	if simpleResponse.ToString() == inValidString {
+		t.Errorf("Result was incorrect with inValidString")
+	} else {
+		t.Log("Done with inValidString")
+	}
+}
--- tests/middleware_test.go
+++ tests/middleware_test.go
@@ -42,6 +42,34 @@ func TestUserCan(t *testing.T) {
             }
         }`,
 	}
+	userDataInValid := map[string]any{
+		"abilities": `{
+		    "roles": {
+                "read": 0,
+                "create": 0,
+                "delete": 0,
+                "update": 0
+            },
+            "users": {
+                "read": 0,
+                "create": 0,
+                "delete": 0,
+                "update": 0
+            },
+            "checklists": {
+                "read": 0,
+                "create": 0,
+                "delete": 0,
+                "update": 0
+            },
+            "checklist_items": {
+                "read": 0,
+                "create": 0,
+                "delete": 0,
+                "update": 0
+            }
+        }`,
+	}
 	if middleware.UserCan(userDataValid, entityName1, action1) {
 		t.Log("Done with userDataValid for users")
 	} else {
@@ -121,8 +149,92 @@ func TestUserCan(t *testing.T) {
 		t.Errorf("Result was incorrect with userDataValid for checklist_items")
 	}
 	if middleware.UserCan(userDataValid, entityName4, action4) {
-		t.Log("Done with userDataValid for checklist_items")
+		t.Log("Done with userDataVchecklist_items")
 	} else {
 		t.Errorf("Result was incorrect with userDataValid for checklist_items")
+	}
+
+	if middleware.UserCan(userDataInValid, entityName1, action1) {
+		t.Errorf("Result was incorrect with userDataValid for users")
+	} else {
+		t.Log("Done with userDataInValid for users")
+	}
+	if middleware.UserCan(userDataInValid, entityName1, action2) {
+		t.Errorf("Result was incorrect with userDataValid for users")
+	} else {
+		t.Log("Done with userDataInValid for users")
+	}
+	if middleware.UserCan(userDataInValid, entityName1, action3) {
+		t.Errorf("Result was incorrect with userDataValid for users")
+	} else {
+		t.Log("Done with userDataInValid for users")
+	}
+	if middleware.UserCan(userDataInValid, entityName1, action4) {
+		t.Errorf("Result was incorrect with userDataValid for users")
+	} else {
+		t.Log("Done with userDataInValid for users")
+	}
+
+	if middleware.UserCan(userDataInValid, entityName2, action1) {
+		t.Errorf("Result was incorrect with userDataValid for roles")
+	} else {
+		t.Log("Done with userDataInValid for roles")
+	}
+	if middleware.UserCan(userDataInValid, entityName2, action2) {
+		t.Errorf("Result was incorrect with userDataValid for roles")
+	} else {
+		t.Log("Done with userDataInValid for roles")
+	}
+	if middleware.UserCan(userDataInValid, entityName2, action3) {
+		t.Errorf("Result was incorrect with userDataValid for roles")
+	} else {
+		t.Log("Done with userDataInValid for roles")
+	}
+	if middleware.UserCan(userDataInValid, entityName2, action4) {
+		t.Errorf("Result was incorrect with userDataValid for roles")
+	} else {
+		t.Log("Done with userDataInValid for roles")
+	}
+
+	if middleware.UserCan(userDataInValid, entityName3, action1) {
+		t.Errorf("Result was incorrect with userDataValid for checklists")
+	} else {
+		t.Log("Done with userDataInValid for checklists")
+	}
+	if middleware.UserCan(userDataInValid, entityName3, action2) {
+		t.Errorf("Result was incorrect with userDataValid for checklists")
+	} else {
+		t.Log("Done with userDataInValid for checklists")
+	}
+	if middleware.UserCan(userDataInValid, entityName3, action3) {
+		t.Errorf("Result was incorrect with userDataValid for checklists")
+	} else {
+		t.Log("Done with userDataInValid for checklists")
+	}
+	if middleware.UserCan(userDataInValid, entityName3, action4) {
+		t.Errorf("Result was incorrect with userDataValid for checklists")
+	} else {
+		t.Log("Done with userDataInValid for checklists")
+	}
+
+	if middleware.UserCan(userDataInValid, entityName4, action1) {
+		t.Errorf("Result was incorrect with userDataValid for checklist_items")
+	} else {
+		t.Log("Done with userDataInValid for checklist_items")
+	}
+	if middleware.UserCan(userDataInValid, entityName4, action2) {
+		t.Errorf("Result was incorrect with userDataValid for checklist_items")
+	} else {
+		t.Log("Done with userDataInValid for checklist_items")
+	}
+	if middleware.UserCan(userDataInValid, entityName4, action3) {
+		t.Errorf("Result was incorrect with userDataValid for checklist_items")
+	} else {
+		t.Log("Done with userDataInValid for checklist_items")
+	}
+	if middleware.UserCan(userDataInValid, entityName4, action4) {
+		t.Errorf("Result was incorrect with userDataValid for checklist_items")
+	} else {
+		t.Log("Done with userDataInValid for checklist_items")
 	}
 }
